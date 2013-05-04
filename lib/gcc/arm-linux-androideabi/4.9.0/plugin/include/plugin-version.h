78574c4457dd82a571e51a14297aee0e21e8e151
Paul Beeler <sparksco@gmail.com>
2013-05-03 23:50:12 -0600

    HEAD is now at e438dc1 Merge remote-tracking branch 'gnu/master' into 4.9.0 Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- lib/gcc/arm-linux-androideabi/4.9.0/plugin/include/plugin-version.h
+++ lib/gcc/arm-linux-androideabi/4.9.0/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "4.9.0";
-static char datestamp[] = "20130503";
+static char datestamp[] = "20130504";
 static char devphase[] = "experimental";
 static char revision[] = "";
 
